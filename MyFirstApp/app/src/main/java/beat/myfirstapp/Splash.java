420f40be91a273dcdd2a8fb229d5c31baf914d2f
FlorisP <Florispauwels@live.nl>
2016-01-05 15:04:46 +0100

    Splash
    Ik hoop dat het goed is, heb wel de main en launcher moeten veranderen,
    ik verander het terug als het verkeerd is

---
+++ MyFirstApp/app/src/main/java/beat/myfirstapp/Splash.java
@@ -0,0 +1,43 @@
+package beat.myfirstapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Splash extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vraag_loading);
+
+        final ImageView iv = (ImageView) findViewById(R.id.imageView);
+        final Animation an = AnimationUtils.loadAnimation(getBaseContext(),R.anim.loading_animation);
+        final Animation an2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
+
+        iv.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                iv.startAnimation(an2);
+                finish();
+                Intent i = new Intent("android.intent.action.MYACTIVITY");
+                startActivity(i);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+
+            }
+        });
+    }
+}
